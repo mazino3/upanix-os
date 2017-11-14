5687a7d5327c57ec070ff164eb7b16d6d3e43802
Prajwala Prabhakar <srinivasa_prajwal@yahoo.co.in>
2017-11-15 05:04:03 +0530

    IEEE header for 802.11 wifi

---
+++ drivers/net/mac80211/IEEE80211.h
@@ -0,0 +1,34 @@
+/*
+ *	Upanix - An x86 based Operating System
+ *  Copyright (C) 2011 'Prajwala Prabhakar' 'srinivasa_prajwal@yahoo.co.in'
+ *                                                                          
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *                                                                          
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *                                                                          
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+#pragma once
+
+#include <stdlib.h>
+
+#define ETH_ALEN  6
+
+class IEEE80211Header 
+{ 
+public:
+  uint16_t _frameControl;
+  uint16_t _durationId;
+	uint8_t _addr1[ETH_ALEN];
+	uint8_t _addr2[ETH_ALEN];
+	uint8_t _addr3[ETH_ALEN];
+	uint16_t _seqCtrl;
+	uint8_t _addr4[ETH_ALEN];
+} PACKED ALIGNED(2);
