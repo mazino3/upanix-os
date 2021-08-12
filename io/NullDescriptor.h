f1048a4261c98eda1261e385c94f335504bd58ab
Prajwala Prabhakar <srinivasa.prajwal@gmail.com>
2021-08-12 22:19:10 +0800

    NullDescriptor which will act like a sink for writes and reads nothing. Used by GUI non-terminal processes to redirect data from stdout to NullDescriptor

---
+++ io/NullDescriptor.h
@@ -0,0 +1,50 @@
+/*
+ *	Upanix - An x86 based Operating System
+ *  Copyright (C) 2011 'Prajwala Prabhakar' 'srinivasa_prajwal@yahoo.co.in'
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#pragma once
+
+#include <IODescriptor.h>
+#include <uniq_ptr.h>
+#include <queue.h>
+#include <mutex.h>
+
+class NullDescriptor : public IODescriptor {
+public:
+  NullDescriptor(int pid, int id);
+
+  int read(char* buffer, int len) override;
+  bool canRead() override {
+    return true;
+  }
+  int write(const char* buffer, int len) override;
+  bool canWrite() override {
+    return true;
+  }
+
+  void seek(int seekType, int offset) override {
+    //no-op;
+  }
+
+  uint32_t getOffset() const override {
+    return 0;
+  }
+
+  uint32_t getSize() const override {
+    return 0;
+  }
+};
